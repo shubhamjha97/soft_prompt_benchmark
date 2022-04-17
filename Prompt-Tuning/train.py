Bug fix in metric computation

Prompt-Tuning/train.py
@@ -40,8 +40,12 @@ def cast_to_int(str):
     except:
         return 0
 
-def compute_metrics(data, metric, tokenizer):
-    predictions_ids = np.argmax(data.predictions, axis=2)
+def compute_metrics(data, metric, tokenizer, config):
+    if config.model_name.startswith("t5"):
+        data_predictions = data.predictions[0]
+    else:
+        data_predictions = data.predictions
+    predictions_ids = np.argmax(data_predictions, axis=2)
     decoded_predictions = tokenizer.batch_decode(predictions_ids, skip_special_tokens=True)
     decoded_labels = tokenizer.batch_decode(data.label_ids, skip_special_tokens=True)
     predictions = [cast_to_int(x) for x in decoded_predictions]
@@ -77,7 +81,7 @@ def train(tokenizer, model, train_dataset, val_dataset, config, metrics):
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=val_dataset,
-        compute_metrics=partial(compute_metrics, metric=metrics, tokenizer=tokenizer),
+        compute_metrics=partial(compute_metrics, metric=metrics, tokenizer=tokenizer, config=config),
         tokenizer=tokenizer,
         data_collator=default_data_collator,
         optimizers=(optimizer, lr_scheduler)