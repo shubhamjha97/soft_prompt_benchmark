5226e63ffc0f4ad86fc16cbc9cb88af72949b437
Shubham Jha <jha1shubham@gmail.com>
2022-04-17 20:32:47 -0400

    Reduce seq length for boolq

--- Prompt-Tuning/dataset_loaders.py
+++ Prompt-Tuning/dataset_loaders.py
@@ -22,6 +22,8 @@ def boolq_loader(tokenizer, soft_prompt_length=0):
             x['target'] = ' '.join(tgt_texts)
         return x
 
+    max_seq_length = 128 # TODO: make a param
+
     raw_dataset = load_dataset('super_glue', 'boolq', split=['train', 'validation[0:100]']) # TODO: remove slicing on val
     # train_dataset, val_dataset = raw_dataset["train"], raw_dataset["validation"]
     train_dataset, val_dataset = raw_dataset[0], raw_dataset[1]
@@ -31,10 +33,10 @@ def boolq_loader(tokenizer, soft_prompt_length=0):
     val_dataset = val_dataset.map(lambda x: boolq_preprocessor(x), batched=False)
 
     # Tokenize dataset
-    train_dataset = tokenize_dataset(train_dataset, tokenizer, 512 - soft_prompt_length, 512 - soft_prompt_length, 'max_length', eval=False)
+    train_dataset = tokenize_dataset(train_dataset, tokenizer, max_seq_length - soft_prompt_length, max_seq_length - soft_prompt_length, 'max_length', eval=False)
     train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
 
-    val_dataset = tokenize_dataset(val_dataset, tokenizer, 512 - soft_prompt_length, 512 - soft_prompt_length, 'max_length', eval=False)
+    val_dataset = tokenize_dataset(val_dataset, tokenizer, max_seq_length - soft_prompt_length, max_seq_length - soft_prompt_length, 'max_length', eval=False)
     val_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
 
     return train_dataset, val_dataset
